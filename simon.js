a3f4c8525284be86ca1c96c23ccbf3bcb11745be
James Brenton <freshlybakedcode@jamesbrenton.co.uk>
2017-03-10 23:14:33 +0100

    Made audio happen

--- simon.js
+++ simon.js
@@ -11,3 +11,43 @@ $(function() {
     $(this).toggleClass('on');
   })
 });
+
+var sequence = [];
+
+function createSequence() {
+  var randomnumber = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
+  sequence.push(randomnumber);
+  console.log(sequence);
+  playSequence();
+}
+
+function playSequence() {
+  for (i=0; i<sequence.length; i++) {
+    switch (sequence[i]) {
+      case 0:
+        var audio = new Audio('./audio/simonSound1.mp3');
+        audio.play();
+        break;
+      case 1:
+        var audio = new Audio('./audio/simonSound2.mp3');
+        audio.play();
+        break;
+      case 2:
+        var audio = new Audio('./audio/simonSound3.mp3');
+        audio.play();
+        break;
+      case 3:
+        var audio = new Audio('./audio/simonSound4.mp3');
+        audio.play();
+        break;
+      default:
+        console.log('Something broke terribly');
+    }
+  }
+}
+
+function getUserInput() {
+
+}
+
+createSequence();
