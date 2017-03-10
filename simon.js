39c511de755eb17429acd2b2d943771b8898af97
James Brenton <freshlybakedcode@jamesbrenton.co.uk>
2017-03-10 23:44:56 +0100

    Pads light up with sound as well

--- simon.js
+++ simon.js
@@ -1,15 +1,11 @@
 $(function() {
-  $('.pad').hover(function() {
-    $(this).toggleClass('lit');
-  })
-
   $('.button-strict').click(function() {
     $(this).prev().toggleClass('lit');
-  })
+  });
 
   $('.switch-slider').click(function() {
     $(this).toggleClass('on');
-  })
+  });
 });
 
 var sequence = [];
@@ -18,7 +14,6 @@ function createSequence() {
   var randomnumber = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
   sequence.push(randomnumber);
   console.log(sequence);
-  playSequence();
 }
 
 function playSequence() {
@@ -26,28 +21,40 @@ function playSequence() {
     switch (sequence[i]) {
       case 0:
         var audio = new Audio('./audio/simonSound1.mp3');
-        audio.play();
+        var relevantPad = '.pad-green';
         break;
       case 1:
         var audio = new Audio('./audio/simonSound2.mp3');
-        audio.play();
+        var relevantPad = '.pad-red';
         break;
       case 2:
         var audio = new Audio('./audio/simonSound3.mp3');
-        audio.play();
+        var relevantPad = '.pad-yellow';
         break;
       case 3:
         var audio = new Audio('./audio/simonSound4.mp3');
-        audio.play();
+        var relevantPad = '.pad-blue';
         break;
       default:
         console.log('Something broke terribly');
     }
+
+    audio.play();
+    setTimeout(function(){
+      $(relevantPad).toggleClass('lit');
+    }, 500);
+    $(relevantPad).toggleClass('lit');
+
   }
 }
 
 function getUserInput() {
+  $('.pad').hover(function() {
+    $(this).toggleClass('lit');
+  });
 
 }
 
 createSequence();
+playSequence();
+getUserInput();
