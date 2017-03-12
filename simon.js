6abf1c5fb177a93914a9dd528f669388bf520032
James Brenton <freshlybakedcode@jamesbrenton.co.uk>
2017-03-12 16:13:30 +0000

    Refactored and working for user input and comparison of 1 array item

--- simon.js
+++ simon.js
@@ -2,13 +2,13 @@ $(function() {
   $('.button-strict').click(function() {
     $(this).prev().toggleClass('lit');
   });
-
   $('.switch-slider').click(function() {
     $(this).toggleClass('on');
   });
 });
 
 var sequence = [];
+var userSequence = [];
 
 function createSequence() {
   var randomnumber = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
@@ -16,45 +16,65 @@ function createSequence() {
   console.log(sequence);
 }
 
-function playSequence() {
+function configureSequence() {
   for (i=0; i<sequence.length; i++) {
-    switch (sequence[i]) {
-      case 0:
-        var audio = new Audio('./audio/simonSound1.mp3');
-        var relevantPad = '.pad-green';
-        break;
-      case 1:
-        var audio = new Audio('./audio/simonSound2.mp3');
-        var relevantPad = '.pad-red';
-        break;
-      case 2:
-        var audio = new Audio('./audio/simonSound3.mp3');
-        var relevantPad = '.pad-yellow';
-        break;
-      case 3:
-        var audio = new Audio('./audio/simonSound4.mp3');
-        var relevantPad = '.pad-blue';
-        break;
-      default:
-        console.log('Something broke terribly');
-    }
-
-    audio.play();
-    setTimeout(function(){
-      $(relevantPad).toggleClass('lit');
-    }, 500);
-    $(relevantPad).toggleClass('lit');
-
+    playAudioAndSound(sequence[i]);
   }
 }
 
-function getUserInput() {
-  $('.pad').hover(function() {
-    $(this).toggleClass('lit');
-  });
-
+function playAudioAndSound(key) {
+  switch (key) {
+    case 0:
+      var audio = new Audio('./audio/simonSound1.mp3');
+      var relevantPad = '#0';
+      break;
+    case 1:
+      var audio = new Audio('./audio/simonSound2.mp3');
+      var relevantPad = '#1';
+      break;
+    case 2:
+      var audio = new Audio('./audio/simonSound3.mp3');
+      var relevantPad = '#2';
+      break;
+    case 3:
+      var audio = new Audio('./audio/simonSound4.mp3');
+      var relevantPad = '#3';
+      break;
+    default:
+      console.log('Something broke terribly');
+  }
+  audio.play();
+  setTimeout(function(){
+    $(relevantPad).toggleClass('lit');
+  }, 500);
+  $(relevantPad).toggleClass('lit');
 }
 
+function getUserInput() {
+  var numberOfAttempts = 0;
+  $('.pad').click(function() {
+    userSequence.push(JSON.parse(this.id));
+      console.log(userSequence);
+      console.log(numberOfAttempts);
+      console.log(userSequence[numberOfAttempts], sequence[numberOfAttempts]);
+
+    playAudioAndSound(userSequence[numberOfAttempts]);
+
+    if(userSequence[numberOfAttempts] === sequence[numberOfAttempts]) {
+      console.log('CORRECT');
+    } else {
+      userLoses(numberOfAttempts);
+    }
+    numberOfAttempts++;
+
+  });
+}
+
+function userLoses(correctGuesses) {
+  console.log(`You fucked up, but got ${correctGuesses} in a row correct.`);
+}
+
+
 createSequence();
-playSequence();
+configureSequence();
 getUserInput();
