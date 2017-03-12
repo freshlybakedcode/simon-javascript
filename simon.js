6d53a78b6268515636592d050df52e4027f854d4
James Brenton <freshlybakedcode@jamesbrenton.co.uk>
2017-03-12 19:20:38 +0000

    Timers now working

--- simon.js
+++ simon.js
@@ -1,24 +1,19 @@
-$(function() {
-  $('.button-strict').click(function() {
-    $(this).prev().toggleClass('lit');
-  });
-  $('.switch-slider').click(function() {
-    $(this).toggleClass('on');
-  });
-});
 
 var sequence = [];
 var userSequence = [];
+var numberOfAttempts;
 
 function createSequence() {
   var randomnumber = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
   sequence.push(randomnumber);
-  console.log(sequence);
+  console.log('Sequence: ', sequence);
 }
 
 function configureSequence() {
-  for (i=0; i<sequence.length; i++) {
-    playAudioAndSound(sequence[i]);
+  for (let i=0; i<sequence.length; i++) {
+      setTimeout( function timer(){
+          playAudioAndSound(sequence[i]);
+      }, i*500 );
   }
 }
 
@@ -50,31 +45,49 @@ function playAudioAndSound(key) {
   $(relevantPad).toggleClass('lit');
 }
 
-function getUserInput() {
-  var numberOfAttempts = 0;
+function userLoses() {
+  console.log(`You fucked up, but got to round ${sequence.length}.`);
+}
+
+function playGame() {
+  userSequence = [];
+  numberOfAttempts = 0;
+  console.log('numberOfAttempts', numberOfAttempts);
+  createSequence();
+  configureSequence();
+}
+
+$(function() {
+  $('.button-strict').click(function() {
+    $(this).prev().toggleClass('lit');
+  });
+  $('.switch-slider').click(function() {
+    $(this).toggleClass('on');
+  });
+
+
   $('.pad').click(function() {
     userSequence.push(JSON.parse(this.id));
-      console.log(userSequence);
-      console.log(numberOfAttempts);
-      console.log(userSequence[numberOfAttempts], sequence[numberOfAttempts]);
+      console.log('userSequence: ', userSequence);
+      console.log('numberOfAttempts', numberOfAttempts);
+      console.log('userSequence[numberOfAttempts]', userSequence[numberOfAttempts], 'sequence[numberOfAttempts]', sequence[numberOfAttempts]);
 
     playAudioAndSound(userSequence[numberOfAttempts]);
 
-    if(userSequence[numberOfAttempts] === sequence[numberOfAttempts]) {
-      console.log('CORRECT');
+    if(userSequence[numberOfAttempts] !== sequence[numberOfAttempts]) {
+      userLoses();
+    } else if (userSequence[numberOfAttempts] === sequence[numberOfAttempts] && userSequence.length !== sequence.length) {
+      console.log('That is the correct pad');
+      numberOfAttempts++;
+    } else if (userSequence[numberOfAttempts] === sequence[numberOfAttempts] && userSequence.length === sequence.length) {
+      console.log('That is the correct sequence! Next round.')
+      setTimeout(function(){
+        playGame();
+      }, 1500);
     } else {
-      userLoses(numberOfAttempts);
+      console.log('Something else totally fucked itself');
     }
-    numberOfAttempts++;
-
   });
-}
+});
 
-function userLoses(correctGuesses) {
-  console.log(`You fucked up, but got ${correctGuesses} in a row correct.`);
-}
-
-
-createSequence();
-configureSequence();
-getUserInput();
+playGame();
