b952956442dc062d0c4f299a6ff84076f04bd7f5
James Brenton <freshlybakedcode@jamesbrenton.co.uk>
2017-03-16 15:40:55 +0000

    Tweaked so webpack will bundle for production

--- README.md
+++ README.md
@@ -12,3 +12,18 @@ My implementation of the Simon game, written in JavaScript.
 - User Story: If I want to restart, I can hit a button to do so, and the game will return to a single step.
 - User Story: I can play in strict mode where if I get a button press wrong, it notifies me that I have done so, and the game restarts at a new random series of button presses.
 - User Story: I can win the game by getting a series of 20 steps correct. I am notified of my victory, then the game starts over.
+
+
+### How to use
+
+1) Clone this repo
+2) Run `npm-install`
+3) Run `npm run dev`
+
+#### Using in development
+After you've installed the npm modules, you should run `npm run devw` to put webpack into watch mode.  Then you're free to modify the /src/ folder files and they will be automatically rebuilt into the /dist folder.
+
+You'll need to load the index.html file from the /dist folder to see what's going on, the /src/ folder files need compilation to work.
+
+#### Using in production
+Once you've completed development, re-run webpack with `npm run prod`.  This will produce a production version of your code which is fully minified and compiled in ES5.
--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,45 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+// const isProduction = env.production === true;
 
-module.exports = {
-	entry: {
-		bundle: path.resolve(__dirname, 'src/js/')
-	},
-	output: {
-		path: path.join(__dirname, 'dist/'),
-		filename: '[name].[chunkhash].js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				use: 'babel-loader',
-				exclude: '/node_modules/'
-			},
-			{
-				test: /\.(scss|sass)$/,
-				use: ExtractTextPlugin.extract({
-					use: ['css-loader', 'sass-loader']
-				})
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: ('src/index.html')
-		}),
-		new ExtractTextPlugin('style.css'),
-    new CopyWebpackPlugin([
-      { from: 'src/audio', to: 'audio' }
-    ], {
-      ignore: [
-        '.DS_Store'
-      ]
-    })
-	]
+module.exports = (env = {}) => {
+  return {
+  	entry: {
+  		bundle: path.resolve(__dirname, 'src/js/')
+  	},
+  	output: {
+  		path: path.join(__dirname, 'dist/'),
+  		filename: '[name].[chunkhash].js'
+  	},
+  	module: {
+  		rules: [
+  			{
+  				test: /\.js$/,
+  				use: 'babel-loader',
+  				exclude: '/node_modules/'
+  			},
+  			{
+  				test: /\.(scss|sass)$/,
+  				use: ExtractTextPlugin.extract({
+  					use: ['css-loader', 'sass-loader']
+  				})
+  			}
+  		]
+  	},
+  	plugins: [
+  		new HtmlWebpackPlugin({
+  			template: ('src/index.html')
+  		}),
+  		new ExtractTextPlugin('style.css'),
+      new CopyWebpackPlugin([
+        { from: 'src/audio', to: 'audio' }
+      ], {
+        ignore: [
+          '.DS_Store'
+        ]
+      })
+  	]
+  }
 };
